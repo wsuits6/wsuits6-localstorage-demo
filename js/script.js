7ef22b7f4a47bc78f2b0d18b7f7541b413a62abd
wsuits6 <wsuits6@gmail.com>
2026-02-01 11:57:15 +0000

    hmmm

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 //=============
 
+const { jsxs } = require("react/jsx-runtime");
+
 //storing Data in local Storage == >> Client Side
 localStorage.setItem("name", "Wsuits6");
 localStorage.setItem("AccountBalance", 50000000000000);
 localStorage.setItem("Age", 19);
 localStorage.setItem("Location", "tamale");
 
+//Company object to push to local Storage
 const companies = {
     companyOne : "WSUITS industries",
     companyTwo : "RedSpectre AI",
-    companyThree : "Hsociety",
-    companyFour : ""
+    companyThree : "Hsociety"
 }
+
+localStorage.setItem("companies", JSON.stringify(companies))

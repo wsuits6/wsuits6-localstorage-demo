74041b2107b237ff0c47c8859a650c6a346188b4
wsuits6 <wsuits6@gmail.com>
2026-02-01 11:45:48 +0000

    added js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//=============
+
+//storing Data in local Storage == >> Client Side
+localStorage.setItem("name", "Wsuits6");
+localStorage.setItem("AccountBalance", 50000000000000);
+localStorage.setItem("Age", 19);
+localStorage.setItem("Location", "tamale");
+
+const companies = {
+    companyOne : "WSUITS industries",
+    companyTwo : "RedSpectre AI",
+    companyThree : "Hsociety",
+    companyFour : ""
+}
